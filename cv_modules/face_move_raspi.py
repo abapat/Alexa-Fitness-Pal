9f5b47231f83bc063225de17ac0f533370f5582e
Abhinandan Dubey <abhinandandubey@live.com>
2017-02-18 05:27:02 -0500

    Some change

--- cv_modules/face_move_raspi.py
+++ cv_modules/face_move_raspi.py
@@ -27,6 +27,9 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+    frame = frame.array
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
