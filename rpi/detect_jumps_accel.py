451cd6a3a16d4b5ef78124668649aaaf164924b8
Abhinandan Dubey <abhinandandubey@live.com>
2017-02-18 23:47:07 -0500

    removed math import

--- rpi/detect_jumps_accel.py
+++ rpi/detect_jumps_accel.py
@@ -1,4 +1,4 @@
-import math
+
 jump_cycle = []
 
 jump_data = []
@@ -30,7 +30,7 @@ def detect_jump(raw_data):
     for jump in jump_sequence:
         current_gyro_z = jump.gyro_z
         if(current_gyro_z < 0 and last_gyro_z > 0):
-            if (math.abs(current_gyro_z + last_gyro_z) > 300):
+            if (abs(current_gyro_z + last_gyro_z) > 300):
                 numJumps += 1
             else:
                 print "Your jumps aren't perfect. Are you jumping really ?"
