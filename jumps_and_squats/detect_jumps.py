0d4fd3a7b4d64a40efe76eddb10ba6d3011331ee
Abhinandan Dubey <abhinandandubey@live.com>
2017-02-19 08:01:21 -0500

    modified to reflect changes as squats

--- jumps_and_squats/detect_jumps.py
+++ jumps_and_squats/detect_jumps.py
@@ -12,6 +12,8 @@ def get_movements():
     numJumps = 0
     frnd_cnt = 0
 
+    ustop = 0
+
     cascPath = 'haarcascade_frontalface_default.xml'
     faceCascade = cv2.CascadeClassifier(cascPath)
 
@@ -47,14 +49,14 @@ def get_movements():
             #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
             if len(faces) > 1:
                 frnd_cnt += 1
-                if(frnd_cnt > 5):
-                    cv2.putText(frame, "Can you ask your friend to move out ?", (100, 50),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                # if(frnd_cnt > 5):
+                #     cv2.putText(frame, "Can you ask your friend to move out ?", (100, 50),
+                #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
             else:
                 frnd_cnt -= 1
                 try:
                     (x, y, w, h) = faces[0]
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 225, 225), 2)
+                    # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 225, 225), 2)
 
                     # new_x = (x+w)/2
                     new_y = (y+w)/2
@@ -63,26 +65,29 @@ def get_movements():
                     movement_y = accumulator[1] - new_y
 
                     if(movement_y > 5):
-                        cv2.putText(frame, "You are moving face upwards", (10, 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                        # cv2.putText(frame, "You are moving face upwards", (10, 20),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                         if(movement_y > 10):
-                            cv2.putText(frame, "Aren't you moving too fast ?", (10, 50),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                            ustop = 0
+                            # cv2.putText(frame, "Aren't you moving too fast ?", (10, 50),
+                            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                             if(movement_dir == 0):
                                 numJumps += 1
                             movement_dir = 1
                     elif(movement_y < -5):
-                        cv2.putText(frame, "You are moving face downwards", (10, 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                        # cv2.putText(frame, "You are moving face downwards", (10, 20),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                         if (movement_y < -10):
-                            cv2.putText(frame, "Aren't you moving too fast ?", (10, 50),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                            ustop = 0
+                            # cv2.putText(frame, "Aren't you moving too fast ?", (10, 50),
+                            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                             if (movement_dir == 1):
                                 numJumps += 1
                             movement_dir = 0
                     else:
-                        cv2.putText(frame, "You are stable", (10, 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                        ustop += 1
+                        # cv2.putText(frame, "You are stable", (10, 20),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
                     # movement = math.sqrt(math.pow(movement_x,2) + math.pow(movement_y,2))
 
@@ -90,11 +95,11 @@ def get_movements():
                 except:
                     pass
 
-        cv2.namedWindow('Video')
-        cv2.imshow('Video', frame)
+        # cv2.namedWindow('Video')
+        # cv2.imshow('Video', frame)
 
-        if time.time() > t_end:
+        if time.time() > t_end or ustop > 10:
             break
 
     cv2.destroyAllWindows()
-    return numJumps
+    return numSquats
